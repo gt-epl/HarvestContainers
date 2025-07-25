ee0edd285f7053cf7aa88d552497b80c357d2973
Anirudh Sarma <anirudh.sarma94@gmail.com>
2025-07-25 17:43:02 -0400

    Details: 1. Removed old workloads: Mutilate, Terasort, img-dnn, fibtest 2. Removed old experiments: Old/ 3. Renamed motivation figure to show improvements over cgroups 4. Removed old tool: img-dnn/parselats

--- TestFramework/Containers/Terasort/get_hadoop.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-wget https://archive.apache.org/dist/hadoop/core/hadoop-2.7.1/hadoop-2.7.1.tar.gz
-tar xzvf hadoop-2.7.1.tar.gz
--- TestFramework/Experiments/Old/memcached/load.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-source ../../Config/SYSTEM.sh
-source ../../Config/MUTILATE.sh
-
-sudo taskset -a -c ${MUTILATE_CORE_RANGE} ${WORKING_DIR}/bin/mutilate -s ${MEMCACHED_SERVER}:11211 -K fb_key -V fb_value -r 1000000 -i fb_ia --loadonly
