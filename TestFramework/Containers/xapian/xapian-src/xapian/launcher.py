5db89274bd467efc08f6d986bc006bf6dfe871a0
Anirudh Sarma <anirudh.sarma94@gmail.com>
2025-07-27 15:43:41 -0400

    fix old name in xapian launcher

--- TestFramework/Containers/xapian/xapian-src/xapian/launcher.py
+++ TestFramework/Containers/xapian/xapian-src/xapian/launcher.py
@@ -80,7 +80,7 @@ def run(server_class=HTTPServer, handler_class=Server, port=20000):
     server_address = ('', port)
     httpd = server_class(server_address, handler_class)
 
-    print(f"img-dnn launcher. Serving on port {port}")
+    print(f"xapian-primary launcher. Serving on port {port}")
     httpd.serve_forever()
 
 if __name__ == "__main__":
