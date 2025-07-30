aaa7a5f70f9e624cd50f279b27bc1b269631ef00
Anirudh Sarma <anirudh.sarma94@gmail.com>
2025-07-30 13:26:34 -0400

    fix latency output from ycsb

--- TestFramework/Containers/MySQL/ycsb/run_ycsb.sh
+++ TestFramework/Containers/MySQL/ycsb/run_ycsb.sh
@@ -20,11 +20,12 @@ echo "[+] Benchmarking reads from MySQL"
 
 cd ycsb-0.17.0
 
+CONSOLE_FILE="/app/results/${TRIAL}.console"
+exec 2>&1 1>$CONSOLE_FILE
+
 RAW_FILE="/app/results/${TRIAL}.raw"
-exec 2>&1 1>$RAW_FILE
 
 # Run YCSB with raw measurements (env defined apriori: e.g., in Dockerfile or ycsb_pod.yaml)
-bin/ycsb run jdbc -P workloads/harvest_read -P db.properties -threads ${YCSB_THREADS} -target ${YCSB_QPS} -p maxexecutiontime=${YCSB_DURATION} -p measurementtype=raw -p measurement.raw.output_file=${OUTPUT_FILE} -s -cp /usr/share/java/mysql-connector-java.jar
-
+bin/ycsb run jdbc -P workloads/harvest_read -P db.properties -threads ${YCSB_THREADS} -target ${YCSB_QPS} -p maxexecutiontime=${YCSB_DURATION} -p measurementtype=raw -p measurement.raw.output_file=${RAW_FILE} -s -cp /usr/share/java/mysql-connector-java.jar
 
 grep SCAN, $RAW_FILE > /app/results/${TRIAL}.out
