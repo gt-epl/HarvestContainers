69708fa2b572473e80b6a02759199b7dce405e90
Anirudh Sarma <anirudh.sarma94@gmail.com>
2025-07-30 17:08:41 -0400

    runall.sh

--- TestFramework/Experiments/runall.sh
+++ TestFramework/Experiments/runall.sh
@@ -6,20 +6,6 @@ dur=60
 
 #./<workload>_runner.sh <trial_num> <num_secondary_workers> <tic> <qps> <dur> <harvest/baseline>
 
-# xapian: ~76 minutes
-start_time=$(date +%s)
-
-for((i=start; i<=end; i++)); do
-  for ((qps=500; qps<=4000; qps+=500)); do
-    ./xapian_runner.sh $i 1 1 $qps $dur baseline
-    ./xapian_runner.sh $i 9 7 $qps $dur harvest
-  done 
-done
-
-end_time=$(date +%s)
-echo "[+] xapian runtime: $((end_time - start_time)) seconds"
-
-mv xapian_config.out /mnt/extra/config/xapian_config.out
 
 # memcached: ~116 minutes
 start_time=$(date +%s)
@@ -38,3 +24,36 @@ end_time=$(date +%s)
 echo "[+] memcached runtime: $((end_time - start_time)) seconds"
 
 mv memcached_config.out /mnt/extra/config/memcached_config.out
+
+# xapian: ~76 minutes
+start_time=$(date +%s)
+
+for((i=start; i<=end; i++)); do
+  for ((qps=500; qps<=4000; qps+=500)); do
+    ./xapian_runner.sh $i 1 1 $qps $dur baseline
+    ./xapian_runner.sh $i 9 7 $qps $dur harvest
+  done 
+done
+
+end_time=$(date +%s)
+echo "[+] xapian runtime: $((end_time - start_time)) seconds"
+
+mv xapian_config.out /mnt/extra/config/xapian_config.out
+
+# mysql
+start_time=$(date +%s)
+
+for((i=start; i<=end; i++)); do
+  for ((qps=1000; qps<=8000; qps+=1000)); do
+    ./mysql_runner.sh $i 1 1 $qps $dur baseline
+    ./mysql_runner.sh $i 9 7 $qps $dur harvest
+  done 
+done
+
+echo "[+] copy results to clabcl1"
+rsync -avz clabsvr:/mnt/extra/results/mysql /mnt/extra/results/
+
+end_time=$(date +%s)
+echo "[+] mysql runtime: $((end_time - start_time)) seconds"
+
+mv mysql_config.out /mnt/extra/config/mysql_config.out
