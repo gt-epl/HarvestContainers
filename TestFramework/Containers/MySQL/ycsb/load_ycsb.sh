3435b5bc82b85526e5bf26e5a9284bcc21dd46fd
Anirudh Sarma <anirudh.sarma94@gmail.com>
2025-07-30 10:04:47 -0400

    dockerize ycsb

--- TestFramework/Containers/MySQL/ycsb/load_ycsb.sh
+++ TestFramework/Containers/MySQL/ycsb/load_ycsb.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-source ../../Config/YCSB.sh
-
-# Uncomment to use local vars
-#JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
-#YCSB_THREADS=32
-
 echo "[+] Loading YCSB workload data into MySQL"
 
 cd ycsb-0.17.0
